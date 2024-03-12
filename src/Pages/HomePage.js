952a31b47502ba4ade845214e22951f27902814f
maainul <mainul080@yahoo.com>
2024-03-12 06:15:26 +0600

    Call Another Server

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { GET_ALL_VEHICLE_LIST } from '../api/api'
+import { GET_ALL_FORM_ATTRIBUTE_LIST, GET_ALL_VEHICLE_LIST } from '../api/api'
 import Sidebar from './../components/layout/Sidebar';
 import Cards from './../components/layout/Cards';
 import Table from './../components/layout/Table';
@@ -12,11 +12,16 @@ const HomePage = () => {
     const [vehicles, setVehicles] = useState([])
     const [error, setError] = useState(null)
 
+    // System Automation Server Data
+    const [formAttributes, setFormAttribute] = useState([])
+
     // Pagination Input Fields
-    const [numberOfPage,setNumberOfPage] = useState(2)
-    const [totalVehicles,setTotalVehicles] = useState(0)
-    const [page,setPage] = useState('1')
-    const [limit,setLimit] = useState('5')
+    const [numberOfPage, setNumberOfPage] = useState(2)
+    const [totalVehicles, setTotalVehicles] = useState(0)
+    const [page, setPage] = useState('1')
+    const [limit, setLimit] = useState('5')
+
+
 
     useEffect(() => {
         const fetchHomePageData = async () => {
@@ -26,13 +31,19 @@ const HomePage = () => {
                 setNumberOfPage(res.data.data.numOfPage)
                 setTotalVehicles(res.data.data.totalVehicleBasedOnQueryObject)
                 setError(null)
+
+                // call to another 
+                const sysRes = await axios.get(GET_ALL_FORM_ATTRIBUTE_LIST)
+                setFormAttribute(sysRes.data.data)
+
+
             } catch (error) {
                 console.log("Error While Getting Expense Types", error)
                 setError('Error: ' + error.message);
             }
         };
         fetchHomePageData();
-    }, [page,limit])
+    }, [page, limit])
 
     return (
 
@@ -59,14 +70,18 @@ const HomePage = () => {
                     {/* Tables of Landing Page */}
                     <div class="relative overflow-x-auto p-10">
                         <div class="relative overflow-x-auto shadow-md bg-white">
-                            <Table data={vehicles}/>
-                            <Pagination 
-                                numberOfPage={numberOfPage} 
-                                setPage={setPage} 
-                                page={page}  
-                                totalVehicles = {totalVehicles}
+                            <Table data={vehicles} />
+                            <Pagination
+                                numberOfPage={numberOfPage}
+                                setPage={setPage}
+                                page={page}
+                                totalVehicles={totalVehicles}
                             />
                         </div>
+                    </div>
+
+                    <div>
+                        {JSON.stringify(formAttributes)}
                     </div>
 
                 </div>
--- src/api/api.js
+++ src/api/api.js
@@ -3,3 +3,6 @@
 export const GET_ALL_VEHICLE_LIST = `${process.env.REACT_APP_API_URL}v1/vehicle/list`
 export const HOME_VEHICLE = `${process.env.REACT_APP_API_URL}v1/vehicle/home`
 export const CREATE_VEHICLE = `${process.env.REACT_APP_API_URL}v1/vehicle/create`
+
+// System Automation Server URL
+export const GET_ALL_FORM_ATTRIBUTE_LIST = `${process.env.REACT_APP_SYS_API_URL}v1/hr/form/attribute/list`
