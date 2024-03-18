36cc7d2a9e5a1eb9ff7966d04d4279c1c496e324
maainul <mainul080@yahoo.com>
2024-03-18 23:52:22 +0600

    yus

--- src/api/api.js
+++ src/api/api.js
@@ -6,3 +6,4 @@ export const CREATE_VEHICLE = `${process.env.REACT_APP_API_URL}v1/vehicle/create
 
 // System Automation Server URL
 export const GET_ALL_FORM_ATTRIBUTE_LIST = `${process.env.REACT_APP_SYS_API_URL}v1/hr/form/attribute/list`
+
