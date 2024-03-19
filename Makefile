f8681c5386743cf1eb03f3f6e7ff781d81f0f6d4
Md.Mainul Hasan <mdevhasan@gmail.com>
2024-03-19 15:13:03 +0600

    UPADTE CODE

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 .PHONY :  docker-build-gate-client
 
 docker build -t ms-gate-client .
+
+
+docker run -d --name ms-gate-client -p 3002:3002 ms-gate-client
--- src/components/forms/UpsertVehicleForm.js
+++ src/components/forms/UpsertVehicleForm.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm,useFieldArray  } from "react-hook-form";
 import InputBox from './../layout/InputBox';
 import TextArea from './../layout/TextArea';
+import axios from 'axios'
+import { CREATE_VEHICLE } from "../../api/api";
+
 
 const UpsertVehicleForm = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const handleRegistration = (data) => console.log(data);
+    const { register, handleSubmit, formState: { errors }} = useForm();
+    const handleRegistration = async(data) => {
+        try {
+            const res = await axios.post(CREATE_VEHICLE,data)
+            console.log("success",res.response.data)
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    }
     const handleError = (errors) => { };
 
     const registerOptions = {
@@ -13,9 +23,25 @@ const UpsertVehicleForm = () => {
             name: { required: "name is required" },
             color: { required: "color is required" },
             model: { required: "model is required" },
-            numberPlate: { required: "numberPlate is required" },
+            numberPlate: { required: "number Plate is required" },
+        },
+         drivers: [
+            {
+            photo: { required: "photo is required" },
+            name: { required: "name is required" },
+            phoneNumber: { required: "phone Number is required" },
+            address: { required: "address is required" },
+        }],
+        visitors: [
+            {
+            name: { required: "name is required" },
+            numberOfPassengers: { required: "number Of Passengers is required" },
+            purpose: { required: "purpose is required" },
+            entryTimes: { required: "entry Times is required" },
         }
+    ]
     };
+
 
     return (
         <div className='m-10 bg-white px-10 py-20 rounded-md'>
@@ -38,15 +64,32 @@ const UpsertVehicleForm = () => {
                     <div className='p-6 rounded-md bg-orange-50 hover:bg-orange-100 hover:cursor-pointer'>
                         <h1 className='text-gray-800 font-semibold'>Driver Info</h1>
                         <InputBox title="Photo" name="drivers[0].photo" id="driverPhoto" borderColor="border-2 border-orange-300 focus:border-orange-600" type="text" register={register} />
-                        {errors && errors.vehicle?.name && <span className="text-red-500 text-sm mt-1">{errors.vehicle?.name.message}</span>}
+                        {/* {errors && errors.drivers?.photo && <span className="text-red-500 text-sm mt-1">{errors.vehicle?.name.photo}</span>} */}
                         <InputBox title="Name" name="drivers[0].name" id="driverName" borderColor="border-2 border-orange-300 focus:border-orange-600" type="text" register={register} />
-                        {errors && errors.vehicle?.name && <span className="text-red-500 text-sm mt-1">{errors.vehicle?.name.message}</span>}
+                        {/* {errors && errors.drivers?.name && <span className="text-red-500 text-sm mt-1">{errors.vehicle?.name.message}</span>} */}
                         <InputBox title="Phone Number" name="drivers[0].phoneNumber" id="driverPhoneNumber" borderColor="border-2 border-oran
                         ge-300 focus:border-orange-600" type="text" register={register} />
-                        {errors && errors.vehicle?.name && <span className="text-red-500 text-sm mt-1">{errors.vehicle?.name.message}</span>}
+                        {/* {errors && errors.drivers?.phoneNumber && <span className="text-red-500 text-sm mt-1">{errors.vehicle?.phoneNumber.message}</span>} */}
                         <TextArea title="Address" name="drivers[0].address" id="driverAddress" borderColor="border-2 border-orange-300 focus:border-orange-600" type="text" register={register} />
-                        {errors && errors.vehicle?.name && <span className="text-red-500 text-sm mt-1">{errors.vehicle?.name.message}</span>}
+                        {/* {errors && errors.drivers?.address && <span className="text-red-500 text-sm mt-1">{errors.vehicle?.address.message}</span>} */}
                     </div>
+
+                   {/* Visitor Info */}
+                    <div className='p-6 rounded-md bg-purple-50 hover:bg-purple-100 hover:cursor-pointer'>
+                        <h1 className='text-gray-800 font-semibold'>Visitor Info</h1>
+                        <InputBox title="Name" name="visitors[0].name" id="visitorName" borderColor="border-2 border-purple-300 focus:border-purple-600" type="text" register={register} />
+                        {/* {errors && errors.visitors?.name && <span className="text-purple-500 text-sm mt-1">{errors.visitors?.name.name}</span>} */}
+                        <InputBox title="Number Of Passengers" name="visitors[0].numberOfPassengers" id="numberOfPassengers" borderColor="border-2 border-purple-300 focus:border-purple-600" type="text" register={register} />
+                        {/* {errors && errors.visitors?.numberOfPassengers && <span className="text-purple-500 text-sm mt-1">{errors.visitors?.numberOfPassengers.message}</span>} */}
+                        <InputBox title="Purpose" name="visitors[0].purpose" id="purpose" borderColor="border-2 border-purple-300 focus:border-purple-600" type="text" register={register} />
+                        {/* {errors && errors.visitors?.purpose && <span className="text-purple-500 text-sm mt-1">{errors.visitors?.purpose.message}</span>} */}
+                       
+                        <InputBox title="reference People" name="visitors[0].referencePeople" id="referencePeople" borderColor="border-2 border-purple-300 focus:border-purple-600" type="text" register={register} />
+                        {/* {errors && errors.visitors?.entryTimes && <span className="text-purple-500 text-sm mt-1">{errors.visitors?.entryTimes.message}</span>} */}
+                        <InputBox title="Entry Times" name="visitors[0].entryTimes" id="entryTimes" borderColor="border-2 border-purple-300 focus:border-purple-600" type="text" register={register} />
+                        {/* {errors && errors.visitors?.entryTimes && <span className="text-purple-500 text-sm mt-1">{errors.visitors?.entryTimes.message}</span>} */}
+                    </div>
+      
                     <div className="flex justify-center my-10 gap-4">
                         <button type="button" className="text-sm font-semibold leading-6 text-white xl: w-72 lg:w-36 sm:w-32 bg-rose-500 rounded-sm shadow-sm hover:bg-rose-400 h-10">
                             Cancel
